220aa618c100c2ecbba8887fa3b5a00f3ee57cf9
Tamilselvan <tamilselvan1998.pdkt@gmail.com>
2026-08-21 13:16:06 +0530

    new files added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+function figmaAssetResolver() {
+  return {
+    name: 'figma-asset-resolver',
+
+    resolveId(id) {
+      if (id.startsWith('figma:asset/')) {
+        const filename = id.replace('figma:asset/', '')
+        return path.resolve(__dirname, 'src/assets', filename)
+      }
+    },
+  }
+}
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/Memoria-Frame/",
-});
+  // GitHub Pages
+  base: '/Memoria-Frame/',
+
+  plugins: [
+    figmaAssetResolver(),
+
+    react(),
+
+    tailwindcss(),
+  ],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
+  assetsInclude: ['**/*.svg', '**/*.csv'],
+})
